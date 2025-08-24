9a95fdbc454e20556f4b219fca51ab35a2692366
steff1437 <steffanali1001@gmail.com>
2025-08-24 02:10:04 +0100

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,84 @@
+
+local config = {
+	--[THESE ARE ALL BUILT IN EXAMPLES BY US, YOU CAN ADD MORE]--
+	{
+		Name = "Fanta", --[NAME]
+		DecalId = "rbxassetid://17767198616", --[WHAT IMAGE APPEARS ON THE SCREEN]
+		Flavors = {
+			{Name = "Cherry", DecalId = "rbxassetid://17767198388", Color = Color3.fromRGB(222, 49, 99), Enabled = true},
+			{Name = "Lime", DecalId = "rbxassetid://17767197948", Color = Color3.fromRGB(0, 255, 0), Enabled = true},
+			{Name = "Orange", DecalId = "rbxassetid://17767197431", Color = Color3.fromRGB(255, 165, 0), Enabled = true},
+			{Name = "Vanilla", DecalId = "rbxassetid://17767196796", Color = Color3.fromRGB(255, 248, 220), Enabled = true},
+			{Name = "Raspberry", DecalId = "rbxassetid://17767197156", Color = Color3.fromRGB(227, 11, 93), Enabled = true}
+		},
+		Color = Color3.fromRGB(255, 140, 0), --[MAIN COLOUR FOR THE MAIN FLAVOR]
+		Glass = "rbxassetid://18414635819" --[OVERALL IMAGE FOR LOGO]
+	},
+	{
+		Name = "Coca-Cola",
+		DecalId = "rbxassetid://17767142191",
+		Flavors = {
+			{Name = "Cherry", DecalId = "rbxassetid://17767141921", Color = Color3.fromRGB(222, 49, 99), Enabled = true},
+			{Name = "Lime", DecalId = "rbxassetid://17767141701", Color = Color3.fromRGB(0, 255, 0), Enabled = true},
+			{Name = "Orange", DecalId = "rbxassetid://17767141452", Color = Color3.fromRGB(255, 165, 0), Enabled = true},
+			{Name = "Vanilla", DecalId = "rbxassetid://17767140822", Color = Color3.fromRGB(255, 248, 220), Enabled = true},
+			{Name = "Raspberry", DecalId = "rbxassetid://17767141163", Color = Color3.fromRGB(227, 11, 93), Enabled = true}
+		},
+		Color = Color3.fromRGB(255, 170, 127),
+		Glass = "rbxassetid://18414636623"
+	},
+	{
+		Name = "Coke Zero Sugar",
+		DecalId = "rbxassetid://17766928489",
+		Flavors = {
+			{Name = "Cherry", DecalId = "rbxassetid://17766927194", Color = Color3.fromRGB(222, 49, 99), Enabled = true},
+			{Name = "Lime", DecalId = "rbxassetid://17766927419", Color = Color3.fromRGB(0, 255, 0), Enabled = true},
+			{Name = "Orange", DecalId = "rbxassetid://17766927637", Color = Color3.fromRGB(255, 165, 0), Enabled = true},
+			{Name = "Vanilla", DecalId = "rbxassetid://17766928101", Color = Color3.fromRGB(255, 248, 220), Enabled = true},
+			{Name = "Raspberry", DecalId = "rbxassetid://17766927890", Color = Color3.fromRGB(227, 11, 93), Enabled = true}
+		},
+		Color = Color3.fromRGB(255, 170, 127),
+		Glass = "rbxassetid://18414636949"
+	},
+	{
+		Name = "Diet Coke",
+		DecalId = "rbxassetid://17767157061",
+		Flavors = {
+			{Name = "Cherry", DecalId = "rbxassetid://17767156887", Color = Color3.fromRGB(222, 49, 99), Enabled = true},
+			{Name = "Lime", DecalId = "rbxassetid://17767154497", Color = Color3.fromRGB(0, 255, 0), Enabled = true},
+			{Name = "Orange", DecalId = "rbxassetid://17767170056", Color = Color3.fromRGB(255, 165, 0), Enabled = true},
+			{Name = "Vanilla", DecalId = "rbxassetid://17767169889", Color = Color3.fromRGB(255, 248, 220), Enabled = true},
+			{Name = "Raspberry", DecalId = "rbxassetid://17767156446", Color = Color3.fromRGB(227, 11, 93), Enabled = true}
+		},
+		Color = Color3.fromRGB(255, 170, 127),
+		Glass = "rbxassetid://18414636408"
+	},
+	{
+		Name = "Sprite",
+		DecalId = "rbxassetid://17767528264",
+		Flavors = {
+			{Name = "Cherry", DecalId = "rbxassetid://17767536779", Color = Color3.fromRGB(222, 49, 99), Enabled = true},
+			{Name = "Lime", DecalId = "rbxassetid://6888858952", Color = Color3.fromRGB(0, 255, 0), Enabled = false},
+			{Name = "Orange", DecalId = "rbxassetid://6888858953", Color = Color3.fromRGB(255, 165, 0), Enabled = false},
+			{Name = "Vanilla", DecalId = "rbxassetid://17767536586", Color = Color3.fromRGB(255, 248, 220), Enabled = true},
+			{Name = "Raspberry", DecalId = "rbxassetid://6888858955", Color = Color3.fromRGB(227, 11, 93), Enabled = false}
+		},
+		Color = Color3.fromRGB(255, 255, 255),
+		Glass = "rbxassetid://126994213257324"
+	},
+	{
+		Name = "Dr. Pepper",
+		DecalId = "rbxassetid://17767178108",
+		Flavors = {
+			{Name = "Cherry", DecalId = "rbxassetid://17767177828", Color = Color3.fromRGB(222, 49, 99), Enabled = true},
+			{Name = "Lime", DecalId = "rbxassetid://6888858962", Color = Color3.fromRGB(0, 255, 0), Enabled = false},
+			{Name = "Orange", DecalId = "rbxassetid://6888858963", Color = Color3.fromRGB(255, 165, 0), Enabled = false},
+			{Name = "Vanilla", DecalId = "rbxassetid://17767177508", Color = Color3.fromRGB(255, 248, 220), Enabled = true}
+		},
+		Color = Color3.fromRGB(170, 85, 0),
+		Glass = "rbxassetid://18414636146"
+	}
+}
+
+return config
+
